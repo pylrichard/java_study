24211fab4d71d2998f1e74e2ecbe561883dea1a0
pylrichard <pylrichard@qq.com>
2019-01-15 20:31:47 +0800

    添加正则表达式工具

--- src/main/java/com/richard/java/study/util/StringUtil.java
+++ src/main/java/com/richard/java/study/util/StringUtil.java
@@ -1,5 +1,14 @@
 package com.richard.java.study.util;
 
+import afu.org.checkerframework.checker.igj.qual.I;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -58,5 +67,131 @@ public class StringUtil {
         String plainText = escapeMatcher.replaceAll("");
 
         return plainText;
+    }
+
+    public static Integer getSameElementCount(String content) {
+        Map<String, Integer> countMap = Maps.newHashMap();
+        Integer totalCount = 0;
+        Integer elementCount = 0;
+        for (int i = 0; i < content.length(); i++) {
+            String element = String.valueOf(content.charAt(i));
+            if (!countMap.containsKey(element)) {
+                countMap.put(element, 1);
+            } else {
+                elementCount = countMap.get(element);
+                countMap.put(element, elementCount + 1);
+            }
+        }
+        for (Map.Entry<String, Integer> entry : countMap.entrySet()) {
+            elementCount = entry.getValue();
+            totalCount += (elementCount > 1 ? elementCount : 0);
+        }
+
+        return totalCount;
+    }
+
+    public static Integer getSameContinuousElementCount(String content, String regex) {
+        Map<String, List<Integer>> indexMap = Maps.newHashMap();
+        Integer count = 0;
+        for (int i = 0; i < content.length(); i++) {
+            String element = String.valueOf(content.charAt(i));
+            if (!indexMap.containsKey(element)) {
+                List<Integer> indexList = Lists.newArrayList();
+                indexList.add(i);
+                indexMap.put(element, indexList);
+            } else {
+                List<Integer> indexList = indexMap.get(element);
+                indexList.add(i);
+            }
+        }
+        for (Map.Entry<String, List<Integer>> entry : indexMap.entrySet()) {
+            String element = entry.getKey();
+            List<Integer> indexList = entry.getValue();
+            HashSet<Integer> indexSet = Sets.newHashSet();
+            Integer size = indexList.size();
+            for (int i = 0; i < size; i++) {
+                if (i == (size - 1)) {
+                    break;
+                }
+                Integer current = indexList.get(i);
+                Integer next = indexList.get(i + 1);
+                Matcher matcher = getMatcher(regex, element);
+                if (matcher.matches()) {
+                    if ((next - current) == 1) {
+                        indexSet.add(i);
+                        indexSet.add(i + 1);
+                    }
+                }
+            }
+            count += indexSet.size();
+        }
+
+        return count;
+    }
+
+    public static Boolean hasDigit(String content) {
+        /*
+            匹配次数
+            *       0次或多次
+            +       1次或多次
+            ?       0次或者1次
+            {n}     n次
+            {n, m}  n到m次
+         */
+        Matcher matcher = getMatcher(".*\\d+.*", content);
+
+        return matcher.matches() ? Boolean.TRUE : Boolean.FALSE;
+    }
+
+    public static Boolean isDigit(String content) {
+        Matcher matcher = getMatcher("[0-9]{1,}", content);
+
+        return matcher.matches();
+    }
+
+    public static Boolean isNumeric(String content) {
+        for (int i = 0; i < content.length(); i++) {
+            if (!Character.isDigit(content.charAt(i))) {
+                return Boolean.FALSE;
+            }
+        }
+
+        return Boolean.TRUE;
+    }
+
+    public static Integer getSymbolCount(String content) {
+        /*
+            /d  [0-9]
+            /D  [^0-9]
+            /w  [a-z0-9]
+            /W  [^a-z0-9]
+         */
+        return getElementCount("[^a-zA-Z0-9]", content);
+    }
+
+    public static Integer getCharCount(String content) {
+        return getElementCount("[a-zA-Z]", content);
+    }
+
+    public static Integer getNumCount(String content) {
+        /*
+            "123"被"\\d"匹配成3个数字，被"\\d+"匹配成1个数字
+         */
+        return getElementCount("\\d", content);
+    }
+
+    private static Integer getElementCount(String regex, String content) {
+        Matcher matcher = getMatcher(regex, content);
+        Integer count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+
+        return count;
+    }
+
+    private static Matcher getMatcher(String regex, String content) {
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(content);
     }
 }
--- src/test/java/com/richard/java/study/util/StringUtilTest.java
+++ src/test/java/com/richard/java/study/util/StringUtilTest.java
@@ -3,9 +3,50 @@ package com.richard.java.study.util;
 import org.junit.Test;
 
 public class StringUtilTest {
+    private static final String CONTENT = "&!@#A4b5cd123";
     @Test
     public void testFilterHtml2PlainText() {
         String srcText = "   id=666<script>alert()</script><style></style>        java    ";
         System.out.println(StringUtil.filterHtml2PlainText(srcText));
+    }
+
+    @Test
+    public void testHasDigit() {
+        System.out.println(StringUtil.hasDigit(CONTENT));
+    }
+
+    @Test
+    public void testIsNumeric() {
+        System.out.println(StringUtil.isNumeric(CONTENT));
+    }
+
+    @Test
+    public void testIsDigit() {
+        System.out.println(StringUtil.isDigit(CONTENT));
+    }
+
+    @Test
+    public void testGetSymbolCount() {
+        System.out.println(StringUtil.getSymbolCount(CONTENT));
+    }
+
+    @Test
+    public void testGetNumCount() {
+        System.out.println(StringUtil.getNumCount(CONTENT));
+    }
+
+    @Test
+    public void testGetCharCount() {
+        System.out.println(StringUtil.getCharCount(CONTENT));
+    }
+
+    @Test
+    public void testGetSameContinuousElementCount() {
+        System.out.println(StringUtil.getSameContinuousElementCount("aaa1111bc22dD3EE", "[0-9a-zA-Z]"));
+    }
+
+    @Test
+    public void testGetSameElementCount() {
+        System.out.println(StringUtil.getSameElementCount("aa11bc22dD3EE"));
     }
 }
